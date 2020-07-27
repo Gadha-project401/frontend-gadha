278bcd6833b112b850dec1c86d710be62b71efad
Ammar <ammar.badwan95@gmail.com>
2020-07-27 23:36:00 +0300

    edited app.js to make merging easier

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import Header from './components/header/header';
 import Body from './components/body/body';
 import Signup from './components/auth/signup';
 import Footer from './components/footer/footer';
-import Dashboard from './components/dashboard/dashboard';
 import { Route } from 'react-router-dom';
 
 const App = props =>{
@@ -16,7 +15,7 @@ const App = props =>{
         <Header/>
 
         <Route path="/signup" exact> <Signup/> </Route>
-        <Route path="/dashboard" component={Dashboard}/>
+
         <Body/>
 
         <Footer/>
