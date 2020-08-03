3907a16ede8c29929b9f736fce4be3a1bf8a5240
israaothman <israaayoub@hotmail.com>
2020-08-03 23:27:24 +0300

    counter

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomePage from './homepage';
+import Mot from '../posts/motivations'
 
 const Body = props => {
   return (
     <>
-    <HomePage/>
+    {/* <HomePage/> */}
+    <Mot/>
     </>
   )
 }
--- src/components/posts/motivations.js
+++ src/components/posts/motivations.js
@@ -1,12 +1,17 @@
-import React , { useEffect} from 'react';
+import React , { useEffect,useState} from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 
 const Posts = props =>{
+    const [counter, setCounter] = useState(0);
 
     const fetchData = e =>{
         e && e.preventDefault();
         props.get();
+    }
+
+    const addition =()=>{
+        setCounter(counter+1);
     }
 
     useEffect(()=>{
@@ -19,11 +24,14 @@ const Posts = props =>{
              {props.posts.posts.map((post,idx)=>{
                  return(
                      <div key={post._id}>
+                         {}
                          {/* <img className="profilePic" src={post.virtualOwner.profilePic}/> */}
                          <h5 className="createdBy" >{post.createdBy}</h5>
                          <p className="createdAt">{post.createdAt}</p>
                          <p className="title">{post.title}</p>
                          <p className="story">{post.story}</p>
+                         <p>{counter}</p>
+                         <button onClick={addition}>like</button>
                      </div>
                  )
              })}
